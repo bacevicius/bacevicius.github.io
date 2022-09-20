15c2975b1580593fde804763fd0c15ef1181919f
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-09-20 15:40:04 +0300

    Navbar background

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           : "fixed w-full h-20 z-[100]"
       }
     >
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+      <div className="bg-gray-100 flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Link href="/">
           <Image
             src="/../public/assets/vidas.png"
