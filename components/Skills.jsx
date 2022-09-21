334ac0256d5b53e9adb3106cd0aeb584a51a66de
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-09-21 11:45:21 +0300

    Skills done

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import React from "react";
-import Html from "../public/assets/skills/html.png";
-import Css from "../public/assets/skills/css.png";
 import Javascript from "../public/assets/skills/javascript.png";
 import ReactImg from "../public/assets/skills/react.png";
-import Github from "../public/assets/skills/github1.png";
+import Python from "../public/assets/skills/python.png";
+import Docker from "../public/assets/skills/docker.png";
+import Java from "../public/assets/skills/java.png";
+import Linux from "../public/assets/skills/linux.png";
+import Vue from "../public/assets/skills/vue.png";
+import Gitlab from "../public/assets/skills/gitlab.png";
 
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full pt-28">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Skills
         </p>
@@ -18,20 +21,20 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Html} width="64px" height="64px" alt="/" />
+                <Image src={Python} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
+                <h3>Python</h3>
               </div>
             </div>
           </div>
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Css} width="64px" height="64px" alt="/" />
+                <Image src={Java} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
+                <h3>Java</h3>
               </div>
             </div>
           </div>
@@ -58,18 +61,48 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={Github} width="64px" height="64px" alt="/" />
+                <Image src={Vue} width="64px" height="64px" alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>Github</h3>
+                <h3>Vue</h3>
               </div>
             </div>
           </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Gitlab} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Gitlab</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Docker} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Docker</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Linux} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Linux</h3>
+              </div>
+            </div>
           </div>
         </div>
+        <p className="my-24">... and whatever you need me to!</p>
       </div>
+    </div>
   );
 };
-
 
 export default Skills;
