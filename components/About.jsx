60b81622ad4cf94458ea4cf863e70f52f154a26a
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-09-21 11:21:43 +0300

    About page done

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const About = () => {
@@ -10,39 +11,37 @@ const About = () => {
             About
           </p>
           <h2 className="py-4">Who I am</h2>
-          <p className="py-2 text-gray-600">I am a COOL developer</p>
           <p className="py-2 text-gray-600">
-            I am a 4th year Computer Science student and a part-time Full Stack
-            Developer at TU Delft X, the Sports and Culture center of my alma
-            mater.
+            I am a graduate of Bachelors of Science in Computer Science and
+            Engineering from TU Delft and a Full Stack Developer at TU Delft X -
+            the Sports and Culture center of my alma mater.
+          </p>
+          <p className="py-2 text-gray-600">
+            As a software engineer, I strive to be open and communicative. I
+            enjoy learning new technologies and do not hesitate to ask for help
+            if I encounter a problem beyond my scope of expertise.
           </p>
 
           <p className="py-2 text-gray-600">
-            I am a 4th year Computer Science student and a part-time Full Stack
-            Developer at TU Delft X, the Sports and Culture center of my alma
-            mater.
+            I am currently interested in the fields of Cloud Computing, DevOps
+            and SRE.
           </p>
 
-          <p className="py-2 text-gray-600">
-            I am a 4th year Computer Science student and a part-time Full Stack
-            Developer at TU Delft X, the Sports and Culture center of my alma
-            mater.
-          </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">
-            Some cool projects that I made:
-          </p>
+          {/* <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
+              Here you can see some cool projects that I worked on.
+            </p>
+          </Link> */}
         </div>
 
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <Image className="rounded-xl"
+          <Image
+            className="rounded-xl"
             src="/../public/assets/face.png"
             alt="/"
-            width="768"
-            height="512"
+            height="616px"
+            width="561px"
           ></Image>
-
-
-          
         </div>
       </div>
     </div>
