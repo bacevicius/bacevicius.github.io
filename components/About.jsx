7e0e9b9d91672df18dc7e1ab4f0152598da85dcb
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-09-24 13:18:23 +0300

    Update asset path

--- components/About.jsx
+++ components/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image
             className="rounded-xl"
-            src="/../public/assets/face.png"
+            src="/../assets/face.png"
             alt="/"
             height="616px"
             width="561px"
